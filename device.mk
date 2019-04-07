e35b1044fc43a474297a7059b6cb501f446d7c1c
bowgotsai <bowgotsai@gmail.com>
2019-04-08 01:35:27 +0530

    X00TD: Adding GSI keys
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66

--- device.mk
+++ device.mk
@@ -118,6 +118,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
     libbthost_if
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
