6ed1ea768ab7d3fe69fcde1fd728e1bebae73103
Valeriano <jneto3000@gmail.com>
2019-09-14 20:44:16 +0000

    X00TD: vndk: merge changes on marlin for Android 10 Reference: https://android.googlesource.com/device/google/marlin/+/refs/tags/android-10.0.0_r2/vndk/Android.mk

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,20 +1,24 @@
+# TODO(b/69526027): DEPRECATE USE OF THIS.
+# USE BOARD_VNDK_VERSION:=current instead.
+
 LOCAL_PATH := $(call my-dir)
 
-ifndef BOARD_VNDK_VERSION
+# b/69526027: This VNDK-SP install routine must be removed. Instead, we must
+# build vendor variants of the VNDK-SP modules.
 
+ifndef BOARD_VNDK_VERSION
+# The libs with "vndk: {enabled: true, support_system_process: true}" will be
+# added VNDK_SP_LIBRARIES automatically. And the core variants of the VNDK-SP
+# libs will be copied to vndk-sp directory.
+# However, some of those libs need FWK-ONLY libs, which must be listed here
+# manually.
 VNDK_SP_LIBRARIES := \
-    libdexfile \
-    libartbase \
-    libziparchive
+    libdexfile_support \
 
 install_in_hw_dir := \
    android.hidl.memory@1.0-impl
 
-ifdef PLATFORM_VNDK_VERSION
 vndk_sp_dir := vndk-sp-$(PLATFORM_VNDK_VERSION)
-else
-vndk_sp_dir := vndk-sp
-endif
 
 define define-vndk-sp-lib
 include $$(CLEAR_VARS)
@@ -27,6 +31,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)$(if $(filter $1,$(install_in_hw_dir)),/hw)
+LOCAL_CHECK_ELF_FILES := false
 include $$(BUILD_PREBUILT)
 
 ifneq ($$(TARGET_2ND_ARCH),)
@@ -41,6 +46,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)$(if $(filter $1,$(install_in_hw_dir)),/hw)
+LOCAL_CHECK_ELF_FILES := false
 include $$(BUILD_PREBUILT)
 endif # TARGET_TRANSLATE_2ND_ARCH is not true
 endif # TARGET_2ND_ARCH is not empty
@@ -56,7 +62,7 @@ VNDK_SP_LIBRARIES := $(filter-out libz,$(VNDK_SP_LIBRARIES))
 
 $(foreach lib,$(VNDK_SP_LIBRARIES),\
     $(eval $(call define-vndk-sp-lib,$(lib))))
-
+	
 install_in_hw_dir :=
 
 include $(CLEAR_VARS)
